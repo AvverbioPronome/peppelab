49a54ba5cc6bc55df24f9f0247802b7f18469292
Giuseppe C <peppecal@gmail.com>
2014-05-07 18:29:07 +0200

    deploy silently.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 ordine=$(security find-internet-password -gs peppelab.altervista.org |& sed 's/.*"\(.*\)"[^"]*$/\1/' | head -n 1)
-ftpsync ignoremask='^\.|^a$|^storiarte$' ./_site ftp://peppelab:${ordine}@peppelab.altervista.org//
+ftpsync -q ignoremask='^\.|^a$|^storiarte$' ./_site ftp://peppelab:${ordine}@peppelab.altervista.org//
